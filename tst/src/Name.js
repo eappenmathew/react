7883962c8b3d0828d664d5af21c6ac2e1c7a5ae8
eappen mathew <allensramp.in@gmail.com>
2020-01-29 21:42:09 +0530

    20200129-2

---
+++ tst/src/Name.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import ReactImage from 'react-image'
+import './App.css';
+import i1 from './public\logo512.png';
+function Name() {
+  return (
+      <div className="Name">
+          <p>allen</p>
+      </div>
+  );
+}
+
+export default Name
--- tst/src/index.js
+++ tst/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Name from './Name';
+
+ReactDOM.render(<Name />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
