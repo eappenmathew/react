fe2cad9e214667653223e12ef910bb9c8db26316
eappen mathew <allensramp.in@gmail.com>
2020-01-29 17:52:55 +0530

    react1

---
+++ test/react.js
@@ -0,0 +1,13 @@
+{/* <script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script> */}
+
+function Person(){
+    return(
+        <div className ="person">
+        <h1>Max</h1>
+        <p>Your age: 28</p>
+        </div>
+    );
+}
+
